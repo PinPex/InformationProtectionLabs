253103fe6b85d67eb5c40a79c096903eefe442af
PinPex <man-scorpion@mail.ru>
2023-11-02 11:39:01 +0700

    Refactored and made sign file

--- lab3.py
+++ lab3.py
@@ -4,29 +4,12 @@ import hashlib
 import random
 import sys
 
-class RSA_keys:
-    def __init__(self, Ni, Di, Ci):
-        self.N = Ni
-        self.D = Di
-        self.C = Ci
-class ElGamal_global_keys:
-    def __init__(self, p, g):
-        self.p = p
-        self.g = g
 class GOST_global_keys:
     def __init__(self, p, q, a):
         self.p = p
         self.q = q
         self.a = a
 
-class ElGamal_keys:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-def generate_ElGamal_keys(global_keys: ElGamal_global_keys):
-    x = get_prime(0, global_keys.p - 1)
-    y = pow_mod(global_keys.g, x, global_keys.p)
-    return ElGamal_keys(x, y)
 
 def generate_ElGamal_global_keys():
     g = 0
@@ -37,36 +20,34 @@ def generate_ElGamal_global_keys():
             break
     while pow_mod(g, q, p) != 1:
         g = random.randint(2, p - 1)
-    return ElGamal_global_keys(p, g)
+    return [p, g]
 
-class ElGamal_signed:
-    def __init__(self, m, r, s):
-        self.m = m
-        self.r = r
-        self.s = s
-def sign_ElGamal(m, keys: ElGamal_keys, glob_keys: ElGamal_global_keys):
-    k = generate_coprime(glob_keys.p - 1)
-    r = pow_mod(glob_keys.g, k, glob_keys.p)
+def generate_ElGamal_keys(global_keys):
+    x = get_prime(0, global_keys[0] - 1)
+    y = pow_mod(global_keys[1], x, global_keys[0])
+    return [global_keys[0], global_keys[1], x, y]
+def sign_ElGamal(m, keys):
+    k = generate_coprime(keys[0] - 1)
+    r = pow_mod(keys[1], k, keys[0])
     h = hashlib.md5(m).hexdigest()
-    h_b = ''.join(str(pow_mod(glob_keys.g, int(i, 16), glob_keys.p)) for i in h)
-    print(f"Before encoding: g^h % p = {h_b}")
-    u = [(int(i, 16) - keys.x * r) % (glob_keys.p - 1) for i in h]
-    s = [(extended_euclidean_algorithm(k, glob_keys.p - 1)[1] * i) % (glob_keys.p - 1) for i in u]
-    return ElGamal_signed(m,r,s)
-def check_sign_ElGamal(m_signed: ElGamal_signed, glob_keys: ElGamal_global_keys, y):
-    h = hashlib.md5(m_signed.m).hexdigest()
-    h_b = ''.join(str(pow_mod(glob_keys.g, int(i, 16), glob_keys.p)) for i in h)
-    print(f"Before decoding: g^h % p = {h_b}")
-    res = ''.join(str(pow_mod(y, m_signed.r, glob_keys.p) * pow_mod(m_signed.r, i, glob_keys.p) % glob_keys.p) for i in m_signed.s)
-    print(f"Decoding: (y^r % p) * (r ^ s % p) % p = {res}")
+    h_b = ''.join(str(pow_mod(keys[1], int(i, 16), keys[0])) for i in h)
+    #print(f"Before encoding: g^h % p = {h_b}")
+    u = [(int(i, 16) - keys[2] * r) % (keys[0] - 1) for i in h]
+    s = [(extended_euclidean_algorithm(k, keys[0] - 1)[1] * i) % (keys[0] - 1) for i in u]
+    return [keys[0], keys[1], keys[2], keys[3], r, s]
+def check_sign_ElGamal(m, keys):
+    h = hashlib.md5(m).hexdigest()
+    h_b = ''.join(str(pow_mod(keys[1], int(i, 16), keys[0])) for i in h)
+    #print(f"Before decoding: g^h % p = {h_b}")
+    res = ''.join(str(pow_mod(keys[3], keys[4], keys[0]) * pow_mod(keys[4], i, keys[0]) % keys[0]) for i in keys[5])
+    #print(f"Decoding: (y^r % p) * (r ^ s % p) % p = {res}")
     return h_b == res
 
 def sign_ElGamal_launch(m):
     global_keys = generate_ElGamal_global_keys()
-    alice_keys = generate_ElGamal_keys(global_keys)
-    signed_text = sign_ElGamal("bla bla bla".encode("utf-8"), alice_keys, global_keys)
-    print(check_sign_ElGamal(signed_text, global_keys, alice_keys.y))
-    pass
+    keys = generate_ElGamal_keys(global_keys)
+    sign = sign_ElGamal(m, keys)
+    return sign
 
 def generate_RSA_keys():
     while True:
@@ -82,27 +63,27 @@ def generate_RSA_keys():
 
     if Ci < 0:
         Ci += Phii
-    return RSA_keys(Ni, Di, Ci)
+    return [Ni, Di, Ci]
 
-def sign_RSA(m, C, N):
+def sign_RSA(m, keys):
     y = hashlib.md5(m).hexdigest()
     y_int = ''.join(str(int(i, 16)) for i in y)
-    print(f"Hash before encoding: {y_int}")
-    s = [pow_mod(int(i, 16), C, N) for i in y]
-    return s
+    #print(f"Hash before encoding: {y_int}")
+    s = [pow_mod(int(i, 16), keys[2], keys[0]) for i in y]
+    return [keys[0], keys[1], keys[2], s]
 
-def check_sign_RSA(m_signed, D, N):
-    y = hashlib.md5(m_signed[0]).hexdigest()
+def check_sign_RSA(m, keys):
+    y = hashlib.md5(m).hexdigest()
     y_int = ''.join(str(int(i, 16)) for i in y)
-    print(f"Hash before decoding: {y_int}")
-    w = ''.join(str(pow_mod(i, D, N)) for i in m_signed[1])
-    print(f'Decoded hash: {w}')
+    #print(f"Hash before decoding: {y_int}")
+    w = ''.join(str(pow_mod(i, keys[1], keys[0])) for i in keys[3])
+    #print(f'Decoded hash: {w}')
     return w == y_int
 
 def sign_RSA_launch(m):
     alice_keys = generate_RSA_keys()
-    m_signed = (m, sign_RSA(m, alice_keys.C, alice_keys.N))
-    print(check_sign_RSA(m_signed, alice_keys.D, alice_keys.N))
+    sign = sign_RSA(m, alice_keys)
+    return sign
 
 def generate_GOST_global_keys():
     q = get_prime(1 << 255, (1 << 256) - 1)
@@ -117,45 +98,74 @@ def generate_GOST_global_keys():
     while not a > 1:
         g = random.randint(1, p - 1)
         a = pow_mod(g, b, p)
-    return GOST_global_keys(p, q, a)
+    return [p, q, a]
 
-def generate_GOST_keys(global_keys: GOST_global_keys):
-    x = random.randrange(1, global_keys.q)
-    y = pow_mod(global_keys.a, x, global_keys.p)
-    return ElGamal_keys(x, y)
+def generate_GOST_keys(global_keys):
+    x = random.randrange(1, global_keys[1])
+    y = pow_mod(global_keys[2], x, global_keys[0])
+    return [global_keys[0], global_keys[1], global_keys[2], x, y]
 
-def sign_GOST(m, keys: ElGamal_keys, glob_keys: GOST_global_keys):
+def sign_GOST(m, keys):
     h = hashlib.md5(m).hexdigest()
     h = int(h, 16)
     r = 0
     s = 0
     while s == 0:
         while r == 0:
-            k = random.randint(1, glob_keys.q - 1)
-            r = pow_mod(glob_keys.a, k, glob_keys.p) % glob_keys.q
-        s = (k * h + keys.x * r) % glob_keys.q
-    return ElGamal_signed(m, r, s)
+            k = random.randint(1, keys[1] - 1)
+            r = pow_mod(keys[2], k, keys[0]) % keys[1]
+        s = (k * h + keys[3] * r) % keys[1]
+    return [keys[0], keys[1], keys[2], keys[3], keys[4], r, s]
 
-def check_sign_GOST(m_signed: ElGamal_signed, glob_keys: GOST_global_keys, y):
-    h = int(hashlib.md5(m_signed.m).hexdigest(), 16)
+def check_sign_GOST(m, keys):
+    h = int(hashlib.md5(m).hexdigest(), 16)
 
-    temp = extended_euclidean_algorithm(h, glob_keys.q)[1]
+    temp = extended_euclidean_algorithm(h, keys[1])[1]
     if temp < 1:
-        temp += glob_keys.q
+        temp += keys[1]
 
-    u1 = (m_signed.s * temp) % glob_keys.q
-    u2 = (-m_signed.r * temp) % glob_keys.q
-    v = ((pow_mod(glob_keys.a, u1, glob_keys.p) * pow_mod(y, u2, glob_keys.p)) % glob_keys.p) % glob_keys.q
-    print(m_signed.r)
-    print(v)
-    return m_signed.r == v
+    u1 = (keys[6] * temp) % keys[1]
+    u2 = (-keys[5] * temp) % keys[1]
+    v = ((pow_mod(keys[2], u1, keys[0]) * pow_mod(keys[4], u2, keys[0])) % keys[0]) % keys[1]
+    return keys[5] == v
 
 def sign_GOST_launch(m):
     global_keys = generate_GOST_global_keys()
-    alice_keys = generate_GOST_keys(global_keys)
-    singed_text = sign_GOST(m, alice_keys, global_keys)
-    print(check_sign_GOST(singed_text, global_keys, alice_keys.y))
+    keys = generate_GOST_keys(global_keys)
+    sign = sign_GOST(m, keys)
+    return sign
+
+def read_from_file(filename):
+    with open(filename) as f:
+        return f.read()
+
+def write_to_file(filename, data):
+    with open(filename, 'w+') as f:
+        f.write(data)
+
+sign_types = {'rsa': check_sign_RSA, 'elgamal': check_sign_ElGamal, 'gost': check_sign_GOST}
+def check_sign_file(filename, ext, sign_type: str, signer_name: str):
+    origin_file = open(filename + "." + ext)
+    sign_file = open(filename + "_sign_" + sign_type.lower() + "_" + signer_name.lower() + ".txt")
+    text = origin_file.read()
+    sign = eval(sign_file.read())
+    return sign_types[sign_type.lower()](text.encode('utf-8'), sign)
+
+def sign_file(filename, ext, sign_type: str, signer_name: str, sign):
+    write_to_file(filename + "_sign_" + sign_type.lower() + "_" + signer_name.lower() + ".txt", str(sign))
+
 def lab3_launch():
-    sign_RSA_launch("bla bla bla".encode('utf-8'))
-    sign_ElGamal_launch("bla bla bla".encode('utf-8'))
-    sign_GOST_launch("bla bla bla".encode('utf-8'))
+    text = read_from_file("unsigned.txt")
+
+    Decode_data_RSA = sign_RSA_launch(text.encode('utf-8'))
+    sign_file("unsigned", "txt", 'rsa', 'alice', Decode_data_RSA)
+    print(check_sign_file("unsigned","txt", 'rsa', 'alice'))
+
+    Decode_data_ElGamal = sign_ElGamal_launch(text.encode('utf-8'))
+    sign_file("unsigned", "txt", 'elgamal', 'alice', Decode_data_ElGamal)
+    print(check_sign_file("unsigned","txt", 'elgamal', 'alice'))
+
+    Decode_data_GOST = sign_GOST_launch(text.encode('utf-8'))
+    sign_file("unsigned", "txt", 'gost', 'alice', Decode_data_GOST)
+    print(check_sign_file("unsigned","txt", 'gost', 'alice'))
+
